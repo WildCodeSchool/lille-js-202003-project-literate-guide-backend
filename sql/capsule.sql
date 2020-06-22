bf4a5a2ed90032d927d8c2fd8b948a1638e65e13
llaulier <lioraloche@yahoo.fr>
2020-06-22 18:11:44 +0200

    sql other insert are added

--- sql/capsule.sql
+++ sql/capsule.sql
@@ -10,3 +10,10 @@ db_yooviz.capsule
 (`id`,`name_capsule
 `,`description`,`duration_video`,`id_poi`,`id_user`,`url_video`) VALUES
 (1,'Les Fusillés de Vauban','Avec BinocleTV, partez à la découverte de Lille.\n\nAujourd\'hui, découvrez la statue des Fusillés, devant l\'esplanade.',84,1,1,'https://www.youtube.com/watch?v=YNdpZG97MYw&list=PLpmketXDUgwEWeA4bfue83eYQmbdc7lJX');
+
+
+INSERT INTO `
+yooviz_db`.`capsule
+`
+(`name_capsule`, `description`, `duration_video`, `id_poi`, `id_user`, `url_video`) VALUES
+('La place du théâtre à Lille', 'Visualisé la place du théâtre', '35', '2', '1', 'https://www.youtube.com/watch?v=iz0RL4Bf65w');
--- sql/poi.sql
+++ sql/poi.sql
@@ -9,3 +9,10 @@ db_yooviz.poi
 `
 (`id`,`name`,`adress`,`latitude`,`longitude`) VALUES
 (1,'Les fusillés lillois','Les fusillés lillois, Square Daubenton, 59800 Lille',50.6379,3.05166);
+
+
+INSERT INTO `
+yooviz_db`.`POI
+`
+(`name`, `adress`, `latitude`, `longitude`) VALUES
+(2, 'La place du Théâtre', 'Boulevard Carnot 59800 Lille', '50.637721', '3.064476');
