5dccc75f56030866fbcef6ca7b880a61b6953d3b
llaulier <lioraloche@yahoo.fr>
2020-06-23 11:57:22 +0200

    back tip modify in yooviz

--- sql/yooviz.sql
+++ sql/yooviz.sql
@@ -5,53 +5,53 @@
 */
 
 CREATE DATABASE yooviz;
-CREATE TABLE tag('id' int NOT NULL AUTO_INCREMENT,'label' varchar
-(100) NOT NULL, 'category' varchar
+CREATE TABLE tag (`id` INT NOT NULL AUTO_INCREMENT,`label` varchar
+(100) NOT NULL, `category`varchar
 (100) NOT NULL,  PRIMARY KEY
 (`id`)),
 
 poi
 (
-    'id' int NOT NULL AUTO_INCREMENT,
-    'poi_name' varchar
+    `id` int NOT NULL AUTO_INCREMENT,
+    `poi_name` varchar
 (100) NOT NULL,
-    'adress' VARCHAR
+    `adress` VARCHAR
 (100) NULL,
-    'latitude' FLOAT NOT NULL,
-    'longitude' FLOAT NOT NULL,
-    'id_poi_tag' INT NULL,
+    `latitude` FLOAT NOT NULL,
+    `longitude` FLOAT NOT NULL,
+    `id_poi_tag` INT NULL,
     PRIMARY KEY
 (`id`)), 
 
 user
 (
-    'id' int NOT NULL AUTO_INCREMENT,
-    'user_name'varchar
+    `id` int NOT NULL AUTO_INCREMENT,
+`user_name`varchar
 (45) NOT NULL, 
-    'password'varchar
+    `password`varchar
 (100) NOT NULL, 
-    'type_user'varchar
-(30) DEFAULT 'tourist' NOT NULL,
-    'type_account'varchar
+    `type_user`varchar
+(30) DEFAULT `tourist` NOT NULL,
+    `type_account`varchar
 (100) NULL,
-    'lastname'varchar
+    `lastname`varchar
 (100) NULL, 
-    'firstname'varchar
+    `firstname`varchar
 (100) NULL, 
-    'email'nvarchar
+    `email`nvarchar
 (255) CHARACTER
 SET utf8
 COLLATE utf8_general_ci NOT NULL, 
-    'sex'tinyint NOT NULL, 
-    'birthday'date NOT NULL,
+    `sex`tinyint NOT NULL, 
+    `birthday`date NOT NULL,
     PRIMARY KEY
 (`id`)), 
 
 capsule
-('id'int NOT NULL AUTO_INCREMENT,'capsule_name' varchar
+(`id`int NOT NULL AUTO_INCREMENT,`capsule_name` varchar
 (100) NOT NULL,
-'description'TEXT NOT NULL,
-'duration_video' INT,'id_poi' INT NOT NULL,'id_user' INT NOT NULL,'link' TEXT NULL,'url_video' nvarchar
+`description`TEXT NOT NULL,
+`duration_video` INT,`id_poi` INT NOT NULL,`id_user` INT NOT NULL,`link` TEXT NULL,`url_video` nvarchar
 (300) CHARACTER
 SET utf8
 COLLATE utf8_general_ci  NOT NULL, PRIMARY KEY
