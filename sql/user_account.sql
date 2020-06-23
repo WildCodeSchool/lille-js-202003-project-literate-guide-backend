9ebf0b091c99a48e018ed9dc9e32d52111da3d8e
llaulier <lioraloche@yahoo.fr>
2020-06-23 11:13:08 +0200

    sql doc are added

---
+++ sql/user_account.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM yooviz_db.user
+LIMIT 0, 1000
+
+-- Date: 2020-06-22 17:52
+*/
+
+INSERT INTO user_account ('id','user_name', 'password', 'type_user','lastname', 'firstname', 'email', 'sex', 'birthday')
+VALUES
+    ('1', 'V4g4b0ndage', '188218', 'tourist', 'Dupuis', 'Anne', 'anne.dupuis@wildmail.fr', '0', '1982-06-22'),
+    ('2', 'Bob', 'x8985y', 'tourist', 'Marley', 'Bob', 'bob@gmail.com', '1', '1976-01-29'),
+    ('3', 'MamaTrotteur', 'yoov17', 'tourist', 'Personna', 'Emma', 'mamatrotteur@gmail.com', '0', '1990-05-23');
---
+++ sql/yooviz.sql
@@ -0,0 +1,59 @@
+/*
+-- Query: creation de la bdd sql
+
+-- Date: 2020-06-22 17:52
+*/
+
+CREATE DATABASE yooviz;
+CREATE TABLE tag('id'int NOT NULL AUTO_INCREMENT,'label'varchar
+(100) NOT NULL, 'category'varchar
+(100) NOT NULL,  PRIMARY KEY
+(`id`)),
+
+poi
+(
+    'id' int NOT NULL AUTO_INCREMENT,
+    'poi_name'varchar
+(100) NOT NULL,
+    'adress'VARCHAR
+(100) NULL,
+    'latitude'FLOAT NOT NULL,
+    'longitude'FLOAT NOT NULL,
+    'poi_tag_id' INT NULL,
+    PRIMARY KEY
+(`id`)), 
+
+user_account
+(
+    'id' int NOT NULL AUTO_INCREMENT,
+    'user_name'varchar
+(45) NOT NULL, 
+    'password'varchar
+(100) NOT NULL, 
+    'type_user'varchar
+(30) DEFAULT 'tourist' NOT NULL,
+    'type_account'varchar
+(100) NULL,
+    'lastname'varchar
+(100) NULL, 
+    'firstname'varchar
+(100) NULL, 
+    'email'nvarchar
+(255) CHARACTER
+SET utf8
+COLLATE utf8_general_ci NOT NULL, 
+    'sex'tinyint NOT NULL, 
+    'birthday'date NOT NULL,
+    PRIMARY KEY
+(`id`)), 
+
+capsule
+('id'int NOT NULL AUTO_INCREMENT,'capsule_name' varchar
+(100) NOT NULL,
+'description'TEXT NOT NULL,
+'duration_video' INT,'id_poi' INT NOT NULL,'id_user' INT NOT NULL,'link' TEXT NULL,'url_video' nvarchar
+(300) CHARACTER
+SET utf8
+COLLATE utf8_general_ci  NOT NULL, PRIMARY KEY
+(`id`));
+
