d7470ecfe78745e9f19f74876a05b159f0df5762
Maxime <maxime.rawinski@gmail.com>
2020-06-29 09:45:22 +0200

    new indentation sql files

--- sql/poi.sql
+++ sql/poi.sql
@@ -5,13 +5,11 @@ LIMIT 0, 1000
 -- Date: 2020-06-22 17:56
 */
 INSERT INTO poi
-    (`id`,`poi_name
-
-`,`adress`,`latitude`,`longitude`)
+    (`id`,`poi_name`,`adress`,`latitude`,`longitude`)
 VALUES
-('1', 'Les fusillés lillois', 'Les fusillés lillois, Square Daubenton, 59800 Lille', '50.6379', '3.05166'),
-('2', 'La place du Théâtre', 'Boulevard Carnot 59800 Lille', '50.637721', '3.064476'),
-('3', 'Maison Natale du Général de Gaulle', '9 Rue Princesse, 59000 Lille', '50.645995', '3.058665'),
-('4', 'Musée de l''Hospice Comtesse', '32 rue de la monnaie 59800 Lille', '50.641409', '3.063411'),
-('5', 'Abbaye de Loos Garden', 'rue des 3 Molettes 59800 Lille', '50.640083', '3.060307'),
-('6', 'Cathedrale Notre-Dame-de-la-Treille', 'Place Gilleson 59800 Lille', '50.640144', '3.062370');
+    ('1', 'Les fusillés lillois', 'Les fusillés lillois, Square Daubenton, 59800 Lille', '50.6379', '3.05166'),
+    ('2', 'La place du Théâtre', 'Boulevard Carnot 59800 Lille', '50.637721', '3.064476'),
+    ('3', 'Maison Natale du Général de Gaulle', '9 Rue Princesse, 59000 Lille', '50.645995', '3.058665'),
+    ('4', 'Musée de l''Hospice Comtesse', '32 rue de la monnaie 59800 Lille', '50.641409', '3.063411'),
+    ('5', 'Abbaye de Loos Garden', 'rue des 3 Molettes 59800 Lille', '50.640083', '3.060307'),
+    ('6', 'Cathedrale Notre-Dame-de-la-Treille', 'Place Gilleson 59800 Lille', '50.640144', '3.062370');
