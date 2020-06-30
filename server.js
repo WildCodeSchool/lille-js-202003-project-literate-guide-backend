a56f568fbbbf89891a26eecb53cee74ae6a72b57
RoxaneDrelon <temporarerare@gmail.com>
2020-06-30 16:19:54 +0200

    Added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const capsules = require("./routes/capsules");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/capsules", capsules);
 
 app.listen(4242, (err) => {
