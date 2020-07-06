9dc79134806af18be097b49f49fc8c2d7e1e8e29
RoxaneDrelon <temporarerare@gmail.com>
2020-07-06 15:47:27 +0200

    Added more tags

--- sql/tag.sql
+++ sql/tag.sql
@@ -1,11 +1,31 @@
 INSERT into tag
     (`id`, `category`, `label`)
 VALUES
-    ('1', 'types', 'Quartier'),
-    ('2', 'types', 'Place'),
-    ('3', 'types', 'Rue'),
-    ('4', 'types', 'Musée'),
-    ('5', 'langue', 'Français'),
-    ('6', 'langue', 'Anglais'),
-    ('7', 'style', 'Humour'),
-    ('8', 'style', 'Enfant');
+    ('1', 'typePoi', 'Quartier'),
+    ('2', 'typePoi', 'Place'),
+    ('3', 'typePoi', 'Rue'),
+    ('4', 'typePoi', 'Musée'),
+    ('5', 'typePoi', 'Monument'),
+    ('6', 'typePoi', 'Parc'),
+    ('7', 'typePoi', 'Street Art'),
+    ('8', 'typePoi', 'Curiosité'),
+    ('9', 'typePoi', 'Fontaine'),
+    ('10', 'typePoi', 'Statue'),
+    ('11', 'typeCapsule', 'Standalone'),
+    ('12', 'typeCapsule', 'Jeu de piste'),
+    ('13', 'typeCapsule', 'Quizz'),
+    ('14', 'thématique', 'Histoire'),
+    ('15', 'thématique', 'Architecture'),
+    ('16', 'thématique', 'Anecdotes'),
+    ('17', 'thématique', 'Beaux-Arts'),
+    ('18', 'thématique', 'Cinéma'),
+    ('19', 'thématique', 'Musique'),
+    ('20', 'thématique', 'Street Art'),
+    ('21', 'thématique', 'Politique'),
+    ('22', 'style', 'Enfant'),
+    ('23', 'style', 'Humour'),
+    ('24', 'langue', 'Français'),
+    ('25', 'langue', 'Anglais');
+    
+    
+    
