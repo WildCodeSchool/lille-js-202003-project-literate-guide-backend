e5425049703c3781a033a374463574dda273088e
RoxaneDrelon <temporarerare@gmail.com>
2020-07-07 15:20:07 +0200

    Done adding course examples

---
+++ sql/course.sql
@@ -0,0 +1,8 @@
+INSERT INTO course
+    (`course_name`, `course_description`, `course_teaser`, `price`, `id_user_course`)
+VALUES
+    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
+    ('Découvrez Lille', "Notre sélection de points d'intérêt est accompagnée de capsules inédites !", 'https://youtu.be/OHuk52K6wDQ', '3', '2'),
+    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
+    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
+    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1');
