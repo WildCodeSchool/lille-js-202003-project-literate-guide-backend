0e461a4639f668a383b6a442f95200c759ad93a8
RoxaneDrelon <temporarerare@gmail.com>
2020-07-09 14:38:32 +0200

    Gave different names for course examples

--- sql/course.sql
+++ sql/course.sql
@@ -5,6 +5,6 @@ INSERT INTO course
 VALUES
     ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
     ('Découvrez Lille', "Notre sélection de points d'intérêt est accompagnée de capsules inédites !", 'https://youtu.be/OHuk52K6wDQ', '3', '2'),
-    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
-    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
-    ('Petit tour de Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1');
+    ('Bienvenue à Lille', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
+    ("C'est le Nord", 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1'),
+    ('Lille Lille Lille !', 'Découvrez Lille via certains des lieux historiques les plus importants de cette capitale du Nord !', 'https://youtu.be/kNqTcWyg15E', '5', '1');
