3555f328d2df131c94278b40b366c100384fda5d
RoxaneDrelon <temporarerare@gmail.com>
2020-07-13 15:33:31 +0200

    More table id

--- controllers/capsules.js
+++ controllers/capsules.js
@@ -15,7 +15,7 @@ const getSimpleCapsules = async (req, res) => {
 const getCapsules = async (req, res) => {
   try {
     const capsule = await db.query(
-      "SELECT c.id, capsule_name, description, duration_video, link, url_video, poi_name, address, latitude, longitude FROM capsule c INNER JOIN poi ON c.id_poi=poi.id"
+      "SELECT c.id, poi.id, capsule_name, description, duration_video, link, url_video, poi_name, address, latitude, longitude FROM capsule c INNER JOIN poi ON c.id_poi=poi.id"
     );
     return res.status(200).json(capsule[0]);
   } catch (e) {
