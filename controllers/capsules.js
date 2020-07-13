270229ff1b8037f932dae6f2b7903312f8234421
RoxaneDrelon <temporarerare@gmail.com>
2020-07-13 14:08:25 +0200

    Added id of the main table called to all GET

--- controllers/capsules.js
+++ controllers/capsules.js
@@ -3,7 +3,7 @@ const { db } = require("../conf");
 const getSimpleCapsules = async (req, res) => {
   try {
     const capsule = await db.query(
-      "SELECT capsule_name, description FROM capsule"
+      "SELECT capsule.id, capsule_name, description FROM capsule"
     );
     return res.status(200).json(capsule[0]);
   } catch (e) {
@@ -15,7 +15,7 @@ const getSimpleCapsules = async (req, res) => {
 const getCapsules = async (req, res) => {
   try {
     const capsule = await db.query(
-      "SELECT capsule_name, description, duration_video, link, url_video, poi_name, address, latitude, longitude FROM capsule c INNER JOIN poi ON c.id_poi=poi.id"
+      "SELECT c.id, capsule_name, description, duration_video, link, url_video, poi_name, address, latitude, longitude FROM capsule c INNER JOIN poi ON c.id_poi=poi.id"
     );
     return res.status(200).json(capsule[0]);
   } catch (e) {
--- controllers/poi.js
+++ controllers/poi.js
@@ -3,7 +3,7 @@ const { db } = require("../conf");
 const getPoi = async (req, res) => {
   try {
     const poi = await db.query(
-      "SELECT poi_name, address, latitude, longitude, label FROM poi p JOIN tag t ON t.id = p.id_poi_tag"
+      "SELECT p.id, poi_name, address, latitude, longitude, label FROM poi p JOIN tag t ON t.id = p.id_poi_tag"
     );
     return res.status(200).json(poi[0]);
   } catch (e) {
